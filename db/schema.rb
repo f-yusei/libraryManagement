d4cf5bab989e6c4cb10461e1ddcd59667e37f4ee
f-yusei <fkys2932@gmail.com>
2025-09-13 19:36:13 +0900

    feat: usersテーブルにnameカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_12_133829) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_13_064954) do
   create_table "sessions", force: :cascade do |t|
     t.integer "user_id", null: false
     t.string "ip_address"
@@ -25,6 +25,7 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_12_133829) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name", null: false
     t.index ["email_address"], name: "index_users_on_email_address", unique: true
   end
 
