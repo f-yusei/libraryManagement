0e65b2015472d1e347a2d7db4ca30bce15de3ee1
f-yusei <fkys2932@gmail.com>
2025-09-13 15:37:32 +0900

    feat: bootstrapの導入と基本的なスタイルの適用

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,26 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
   def create
+    @user = User.new(user_params)
+    if @user.save
+      # 保存成功処理
+      # セッションのリセット
+      # ログイン
+      # flash
+      # リダイレクト
+    else
+      render "new", status: :unprocessable_content
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end
 end
