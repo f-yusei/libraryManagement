19bf0b2eaf64b00ee73248c6be3340b9df4c89ba
f-yusei <fkys2932@gmail.com>
2025-09-14 15:27:27 +0900

    feat: 中間テーブルとのリレーションを設定

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,7 @@
 class Book < ApplicationRecord
+  has_many :book_authors, dependent: :destroy
+  has_many :authors, through: :book_authors
+
+  has_many :lendings
+  has_many :users, through: :lendings
 end
