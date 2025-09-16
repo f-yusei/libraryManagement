977ec93cf382463aa8234bd755b87a9cc1e5d4af
f-yusei <fkys2932@gmail.com>
2025-09-17 00:08:24 +0900

    feat: 書籍登録機能を追加し、著者名のバリデーションを実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -11,4 +11,36 @@ class BooksController < ApplicationController
   def edit
     @book = Book.find(params[:id])
   end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    Book.transaction do
+      @book = Book.new(book_params)
+      # ここで著者がパラメータに含まれていなかった時点でunprocessable_entityを返す
+      if @book.author_names.blank?
+        @book.errors.add(:authors, "を入力してください")
+        render :new, status: :unprocessable_entity
+        return
+      end
+      @book.author_names.split(",").map(&:strip).each do |name|
+        author = Author.find_or_create_by!(name: name)
+        @book.authors << author
+      end
+
+      if @book.save
+        redirect_to @book, flash: { success: "本の登録が完了しました。" }
+      else
+        render :new, status: :unprocessable_entity
+      end
+    end
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :isbn, :published_year, :publisher, :stock_count, :author_names)
+  end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,10 +1,15 @@
 class Book < ApplicationRecord
+  attr_accessor :author_names
   has_many :book_authors, dependent: :destroy
   has_many :authors, through: :book_authors
 
   has_many :lendings
   has_many :users, through: :lendings
   before_validation :strip_isbn_hyphens
+
+  validates :title, presence: true
+  validates :isbn, presence: true, uniqueness: true
+  validates :author_names, presence: true
 
   scope :search, ->(query) {
     return all.preload(:authors) if query.blank?
