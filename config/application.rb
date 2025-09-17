f917487010e62aee9753b4d322aa7ca0ab75fdce
f-yusei <fkys2932@gmail.com>
2025-09-17 14:54:51 +0900

    feat: アプリケーション初期化時にシードデータをロードする設定を追加

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,10 @@ module LibraryManagement
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.after_initialize do
+      if Rails.env.production?
+        Rails.application.load_seed
+      end
+    end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,34 +1,42 @@
-# db/seeds.rb
+# Users
+alice = User.find_or_create_by!(email_address: "alice@example.com") do |user|
+  user.name = "Alice"
+  user.password = "password"
+  user.password_confirmation = "password"
+  user.admin = true
+end
 
-# ---- ユーザー ----
-users = User.create!([
-  { name: "Alice", email_address: "test1@example.com", password: "password" },
-  { name: "Bob",   email_address: "test2@example.com",   password: "password" },
-  { name: "Carol", email_address: "test3@example.com", password: "password" }
-])
+bob = User.find_or_create_by!(email_address: "bob@example.com") do |user|
+  user.name = "Bob"
+  user.password = "password"
+  user.password_confirmation = "password"
+  user.admin = false
+end
 
-# ---- 著者 ----
-authors = Author.create!([
-  { name: "Yukihiro Matsumoto" },
-  { name: "David Heinemeier Hansson" },
-  { name: "Martin Fowler" }
-])
+carol = User.find_or_create_by!(email_address: "carol@example.com") do |user|
+  user.name = "Carol"
+  user.password = "password"
+  user.password_confirmation = "password"
+  user.admin = false
+end
 
-# ---- 書籍 ----
-books = Book.create!([
-  { title: "Programming Ruby", isbn: "9781234567890", published_year: Date.new(2008, 1, 1),
-    publisher: "O'Reilly Japan", stock_count: 3 },
-  { title: "Agile Web Development with Rails", isbn: "9781234567891", published_year: Date.new(2010, 6, 1),
-    publisher: "Pragmatic Bookshelf", stock_count: 2 },
-  { title: "Refactoring", isbn: "9781234567892", published_year: Date.new(2018, 11, 1),
-    publisher: "Addison-Wesley", stock_count: 1 }
-])
+# Authors
+natsume = Author.find_or_create_by!(name: "夏目漱石")
+dazai   = Author.find_or_create_by!(name: "太宰治")
 
-# ---- 書籍と著者の関連付け ----
-BookAuthor.create!([
-  { book: books[0], author: authors[0] }, # Programming Ruby ← Matz
-  { book: books[1], author: authors[1] }, # Agile Web Dev ← DHH
-  { book: books[2], author: authors[2] }, # Refactoring ← Fowler
-  # 複数著者パターン
-  { book: books[1], author: authors[2] }  # Agile Web Dev ← Fowler も
-])
+# Books
+book1 = Book.find_or_create_by!(isbn: "9781234567890") do |book|
+  book.title = "吾輩は猫である"
+  book.publisher = "岩波書店"
+  book.published_year = Date.new(1905, 1, 1)
+  book.stock_count = 3
+end
+book1.authors << natsume unless book1.authors.include?(natsume)
+
+book2 = Book.find_or_create_by!(isbn: "9789876543210") do |book|
+  book.title = "人間失格"
+  book.publisher = "新潮社"
+  book.published_year = Date.new(1948, 1, 1)
+  book.stock_count = 2
+end
+book2.authors << dazai unless book2.authors.include?(dazai)
