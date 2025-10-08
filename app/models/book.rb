887e3897b7c8df9de4ff2b9a277df90649b1fc36
f-yusei <fkys2932@gmail.com>
2025-10-08 23:33:59 +0900

    refactor: 不要なメソッドを削除し、貸出中の本の削除に関するテストを追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -8,26 +8,6 @@ class Book < ApplicationRecord
 
   validates :title, presence: true
   validates :isbn, presence: true, uniqueness: true
-  validate :must_have_authors
-
-  attr_accessor :author_names_string
-
-  def can_be_destroyed?
-    lendings.unreturned.empty?
-  end
-
-  def assign_authors_from_string(names_string)
-    self.author_names_string = names_string
-    return if names_string.blank?
-
-    names = names_string.split(",").map(&:strip).reject(&:blank?).uniq
-    return if names.empty?
-
-    transaction do
-      authors = names.map { |name| Author.find_or_create_by!(name: name) }
-      self.authors = authors
-    end
-  end
 
   scope :search, ->(query) {
     return all.preload(:authors) if query.blank?
@@ -41,25 +21,20 @@ class Book < ApplicationRecord
   # ビジネスルール: 貸出中の本は削除できない
   before_destroy :ensure_not_lent_out
 
+  def can_be_destroyed?
+    lendings.unreturned.empty?
+  end
+
   private
-
-  def must_have_authors
-    if author_names_string.present?
-      names = author_names_string.split(",").map(&:strip).reject(&:blank?)
-      errors.add(:authors, "を入力してください") if names.empty?
-    elsif authors.empty? && persisted?
-      errors.add(:authors, "を入力してください")
-    end
-  end
-
-  def strip_isbn_hyphens
-    self.isbn = isbn.delete("-") if isbn.present?
-  end
 
   def ensure_not_lent_out
     unless can_be_destroyed?
       errors.add(:base, "貸出中の本は削除できません")
       throw(:abort)
     end
+  end
+
+  def strip_isbn_hyphens
+    self.isbn = isbn.delete("-") if isbn.present?
   end
 end
--- test/controllers/books_controller_test.rb
+++ test/controllers/books_controller_test.rb
@@ -6,4 +6,55 @@ class BooksControllerTest < ActionDispatch::IntegrationTest
     assert_template "books/index"
     assert_response :success
   end
+
+  test "destroy requires admin authentication" do
+    book = books(:one)
+
+    # 未ログインユーザー
+    delete book_path(book)
+    assert_redirected_to new_session_path
+
+    # 一般ユーザー
+    sign_in_as(users(:one))
+    delete book_path(book)
+    assert_redirected_to root_path
+    assert_equal "アクセス権限がありません", flash[:danger]
+  end
+
+  test "destroy redirects with success when book can be destroyed" do
+    book = books(:one)
+    # 既存の貸出データをクリア
+    book.lendings.destroy_all
+
+    sign_in_as(users(:admin)) # 管理者でログイン
+
+    assert_difference("Book.count", -1) do
+      delete book_path(book)
+    end
+
+    assert_redirected_to root_path
+    assert_equal "本の削除が完了しました。", flash[:success]
+  end
+
+  test "destroy redirects with error when book cannot be destroyed" do
+    book = books(:one)
+    user = users(:one)
+    sign_in_as(users(:admin))
+
+    # 貸出中にする
+    Lending.create!(
+      book: book,
+      user: user,
+      checked_out_at: Time.current,
+      due_date: 2.weeks.from_now,
+      returned_at: nil
+    )
+
+    assert_no_difference("Book.count") do
+      delete book_path(book)
+    end
+
+    assert_redirected_to root_path
+    assert_equal "貸出中の本は削除できません", flash[:danger]
+  end
 end
