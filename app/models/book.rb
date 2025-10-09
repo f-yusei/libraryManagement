1c4a0d984209eef4350fc459bffc20ba4e352545
f-yusei <fkys2932@gmail.com>
2025-10-10 02:33:17 +0900

    feat: 本の作成機能に著者とタグの設定を追加し、関連するバリデーションを実装

--- app/models/book.rb
+++ app/models/book.rb
@@ -12,13 +12,54 @@ class Book < ApplicationRecord
   validates :title, presence: true
   validates :isbn, presence: true, uniqueness: true
 
-  scope :search, ->(query) {
-    return all.preload(:authors) if query.blank?
+  # シンプルなバリデーション：保存時に著者が存在するかチェック
+  validate :must_have_authors_on_save
 
-    joins(:authors)
-      .where("books.title LIKE :q OR books.isbn LIKE :q OR authors.name LIKE :q", q: "%#{query}%")
+  attr_accessor :author_names_string, :tag_names_string
+
+  def assign_authors(names_string)
+    # 著者名文字列を保存（バリデーション用）
+    self.author_names_string = names_string
+
+    names = parse_names(names_string)
+    if names.empty?
+      self.authors = []
+    else
+      authors = names.map { |name| find_or_create_author(name) }
+      self.authors = authors
+    end
+  end
+
+  def assign_tags(names_string)
+    # タグ名文字列を保存
+    self.tag_names_string = names_string
+
+    # タグはオプショナルなので、空文字列やnilの場合は全てクリア
+    if names_string.blank?
+      self.tags = []
+      return
+    end
+
+    names = parse_names(names_string)
+    if names.empty?
+      self.tags = []
+    else
+      tags = names.map { |name| find_or_create_tag(name) }
+      self.tags = tags
+    end
+  end
+
+  scope :search, ->(query) {
+    return all.preload(:authors, :tags) if query.blank?
+
+    # LEFT JOINを使用して、著者やタグがない本も含める
+    left_joins(:authors, :tags)
+      .where(
+        "books.title LIKE :q OR books.isbn LIKE :q OR authors.name LIKE :q OR tags.name LIKE :q",
+        q: "%#{query}%"
+      )
       .distinct
-      .preload(:authors)
+      .preload(:authors, :tags)
   }
 
   # ビジネスルール: 貸出中の本は削除できない
@@ -30,14 +71,37 @@ class Book < ApplicationRecord
 
   private
 
+  def must_have_authors_on_save
+    if authors.empty?
+      errors.add(:authors, "を入力してください")
+    end
+  end
+
+  def parse_names(names_string)
+    return [] if names_string.nil?
+    names_string.split(",").map(&:strip).reject(&:blank?).uniq
+  end
+
+  def find_or_create_author(name)
+    Author.find_or_create_by(name: name)
+  rescue ActiveRecord::RecordNotUnique
+    Author.find_by!(name: name)
+  end
+
+  def find_or_create_tag(name)
+    Tag.find_or_create_by(name: name)
+  rescue ActiveRecord::RecordNotUnique
+    Tag.find_by!(name: name)
+  end
+
+  def strip_isbn_hyphens
+    self.isbn = isbn.delete("-") if isbn.present?
+  end
+
   def ensure_not_lent_out
     unless can_be_destroyed?
       errors.add(:base, "貸出中の本は削除できません")
       throw(:abort)
     end
-  end
-
-  def strip_isbn_hyphens
-    self.isbn = isbn.delete("-") if isbn.present?
   end
 end
--- app/models/book_author.rb
+++ app/models/book_author.rb
@@ -1,6 +1,4 @@
 class BookAuthor < ApplicationRecord
   belongs_to :book
   belongs_to :author
-
-  validates :book_id, presence: true
 end
